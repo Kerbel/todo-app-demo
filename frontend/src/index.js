b73b1d1d67cc664e23d1f158b14ef548294d4f7b
Kerbel <Avivk6@gmail.com>
2022-09-23 13:32:26 -0400

    Revert "Helios installation"
    This reverts commit 0912be8c8fabb18ea3483b4c4f0854d314e20c0d.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,6 @@ import { ThemeProvider } from "@chakra-ui/core";
 import Header from "./Components/Header";
 import Todos from "./Components/Todos";
 
-const { initialize } = require('@heliosphere/web-sdk');
-
-initialize({
-    apiToken: 'a9665ac9688fe681e2d1',
-    serviceName: 'frontend',
-    enable: true,
-    environment: 'local_aviv'
-});
-
-
 function App() {
   return (
     <ThemeProvider>
